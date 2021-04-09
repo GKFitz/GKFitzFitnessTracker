a89277044b98ac6ffdbb792baea4337bd9008e0b
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-09 01:02:22 -0400

    added the workout.js model code

---
+++ models/workout.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workOutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: "Enter Exercise Type",
+        },
+      name: {
+        type: String,
+        required: "Enter Exercise Type",
+
+      },
+      duration: {
+        type: Number,
+        required: "Enter Exercise Type",
+      },
+      weight: {
+        type: Number,
+        
+      },
+      reps: {
+        type: Number,
+
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+        }
+
+    }
+  ]
+});
